012b0a639561279bbcb58946cd2dbee0017e4bd5
enricovanghetti <enrivangus@gmail.com>
2024-11-27 21:28:42 +0100

    Update lidardriver.h

--- LidarDriver/include/lidardriver.h
+++ LidarDriver/include/lidardriver.h
@@ -9,32 +9,32 @@
 class LidarDriver
 {
 	private:
-		// Costanti di default
-		static constexpr int STD_DIM = 180;        		// Risoluzione angolare standard
-		static constexpr int BUFFER_DIM = 10;     		// Dimensione del buffer circolare
+		//costanti di default
+		static constexpr int STD_DIM = 180;        		//risoluzione angolare standard
+		static constexpr int BUFFER_DIM = 10;     		//dimensione del buffer circolare
 		
-		int scan_dim;                              		// Dimensione della scansione
-		std::array<std::vector<double>, BUFFER_DIM> buffer;   	// Buffer circolare
-		int front = -1;                            		// Testa del buffer 
-		int rear = -1;                             		// Coda del buffer
+		int scan_dim;                              		//dimensione della scansione
+		std::array<std::vector<double>, BUFFER_DIM> buffer;   	//buffer circolare
+		int front = -1;                            		//testa del buffer 
+		int rear = -1;                             		//coda del buffer
 
-		// Metodi 
-		void update_front();                       		// Aggiorna front
-		void update_rear();                        		// Aggiorna rear
+		//metodi 
+		void update_front();                       		//aggiorna front
+		void update_rear();                        		//aggiorna rear
 
 	public:
-		// Costruttori
-		LidarDriver();                             		// Costruttore di default
-		LidarDriver(double resolution);           		// Costruttore con risoluzione personalizzata
+		//costruttori
+		LidarDriver();                             		//costruttore di default
+		LidarDriver(double resolution);           		//costruttore con risoluzione personalizzata
 
-		// Funzioni 
-		void new_scan(std::vector<double> inputScan); 		// Inserisce una nuova scansione nel buffer
-		std::vector<double> get_scan();               		// Restituisce la scansione meno recente e la rimuove
-		void clear_buffer();                          		// Pulisce il buffer
-		double get_distance(double angle) const;      		// Restituisce la distanza corrispondente a un dato angolo
+		//funzioni 
+		void new_scan(std::vector<double> inputScan); 		//inserisce una nuova scansione nel buffer
+		std::vector<double> get_scan();               		//restituisce la scansione meno recente e la rimuove
+		void clear_buffer();                          		//pulisce il buffer
+		double get_distance(double angle) const;      		//restituisce la distanza corrispondente a un dato angolo
 
-		// Overload operatore <<
+		//overload operatore <<
 		friend std::ostream& operator<<(std::ostream& out, const LidarDriver& lidar);
 };
 
-#endif // LIDARDRIVER_H
+#endif //LIDARDRIVER_H
