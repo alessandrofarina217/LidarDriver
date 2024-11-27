6525abf45becb1deda4dfab45e752615a67f11f3
Alessandro Farina <185652223+alessandrofarina217@users.noreply.github.com>
2024-11-27 19:23:23 +0100

    Update lidardriver.h
    Modifica di buffer: ora è un array di vector. Cambio in quanto la dimensione di buffer deve essere costante (da consegna sembra costante per ogni oggetto).

--- lidardriver.h
+++ lidardriver.h
@@ -2,6 +2,7 @@
 #define LIDARDRIVER_H
 
 #include <vector>
+#include <array>
 #include <iostream>
 #include <stdexcept> 
 
@@ -13,7 +14,7 @@ class LidarDriver
 		static constexpr int BUFFER_DIM = 10;     		// Dimensione del buffer circolare
 		
 		int SCAN_DIM;                              		// Dimensione della scansione
-		std::vector<std::vector<double>> buffer;   		// Buffer circolare
+		std::array<std::vector<double>, BUFFER_DIM> buffer;   	// Buffer circolare
 		int front = -1;                            		// Testa del buffer 
 		int rear = -1;                             		// Coda del buffer
 
